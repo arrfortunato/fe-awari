8e212b4f81be24f8ad4e35f3d49367e8064e190d
André Fortunato <rrandsky@gmail.com>
2024-05-21 20:37:41 -0300

    add aula 7 improvements

--- aula-7/src/App.jsx
+++ aula-7/src/App.jsx
@@ -1,7 +1,9 @@
+import Title from "./Title";
+
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Title text="Hello world!"/>
     </div>
   );
 }
---
+++ aula-7/src/Title.jsx
@@ -0,0 +1,9 @@
+function Title({ text }) {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
+}
+
+export default Title;
